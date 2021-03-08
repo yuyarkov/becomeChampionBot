c91dbcb737a2d6562070ae3e473d04bf4478edea
alexandr shumeyko <shum_ont@mail.ru>
2021-03-08 23:26:52 +0300

    refactor

--- build-release-deploy-shum.sh
+++ build-release-deploy-shum.sh
@@ -1,3 +1,3 @@
 mvn clean package
-heroku container:push -a dev-telegram-champion-bot  worker
-heroku container:release -a dev-telegram-champion-bot worker
+heroku container:push -a dev-become-champion-tg-bot  worker
+heroku container:release -a dev-become-champion-tg-bot worker
